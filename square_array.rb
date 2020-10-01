51ded23026347c01ad04644597b5ad5643ba7954
turpp <white.tripp@icloud.com>
2020-10-01 13:11:04 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  new_num=[]
+  array.each do |sq_num|
+     new_num << sq_num ** 2
+  
+  end 
+  return new_num
 end
+
+
+
+
+# numbers = [2,4,6,8]
+# numbers.each do |sq_num|
+#    sq_num * sq_num
+
+# end 
